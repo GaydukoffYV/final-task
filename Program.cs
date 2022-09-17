d6c119e8ee2f7e69300e1bd02237613b60fae22d
Yury <gaiducov@mail.ru>
2022-09-17 22:59:24 +0300

    add file Program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write($"Задйте массив, вводя данные через запятую: ");
+string info = Console.ReadLine();
+string[] array = info.Split(',').ToArray();
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+ 
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
